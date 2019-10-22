a0e68cf4768e508116dec0338e40426fae52c6e0
Davidson Ferreira <davidson.tk@hotmail.com>
2019-10-22 00:43:49 -0300

    retirando temporariamente o spring security

--- src/main/java/br/com/xlo/security/WebSecurity.java
+++ src/main/java/br/com/xlo/security/WebSecurity.java
@@ -34,15 +34,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager(),userDetailsService))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-                 //this disables session creation on Spring Security
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        
+        http.csrf().disable();
+                    http.authorizeRequests().antMatchers("/").permitAll();
+//        http.cors().and().csrf().disable().authorizeRequests();
+//                .antMatchers(HttpMethod.POST, "/login").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilter(new JWTAuthenticationFilter(authenticationManager(),userDetailsService))
+//                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+//                 //this disables session creation on Spring Security
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        
 
     }
 
