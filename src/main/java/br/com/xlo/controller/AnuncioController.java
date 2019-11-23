90268d63cdacf2a88b2938f2bf5ffeef281940a9
Davidson Ferreira <davidson.tk@hotmail.com>
2019-11-23 18:20:14 -0300

    Começando a salvar imagens no computador

--- src/main/java/br/com/xlo/controller/AnuncioController.java
+++ src/main/java/br/com/xlo/controller/AnuncioController.java
@@ -13,8 +13,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -46,5 +49,11 @@ public class AnuncioController {
         return opcionaisRepository.findAll();
     }
     
+    
+    @PostMapping("salvarImagensAnuncio")
+    public void salvarImagensAnuncio(@RequestParam List<MultipartFile> fotos){
+        
+       
+    }
     
 }
